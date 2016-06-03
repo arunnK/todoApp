e03d28935fb1ece25801cda801acee251bcb209d
arunnK <arunkumar7617@gmail.com>
2016-06-03 15:33:01 +0530

    Added example for controller.index method

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,19 @@
 RSpec.describe UsersController, type: :controller  do
-
   before do
     @valid_params={first_name: 'Arun', last_name: 'Kumar', email: 'akc@gmail.com', phone: '9876456783',
                password: '3546twrtet!!32', password_confirmation: '3546twrtet!!32'}
     @invalid_params={first_name: 'Arun', last_name: 'Kumar', email: nil, phone: '9876456783',
                password: '3546twrtet!!32', password_confirmation: '3546twrtet!!32'}
+    user1={first_name: 'Arun',last_name: 'Kumar', email: 'abc@g.com', phone:'4566544567'}
+    user2={first_name: 'Ajay',last_name: 'Negi', email: 'abcd@g.com', phone:'4565544567'}
+    @users_list=[user1,user2]
+  end
+
+  describe '#index' do
+    it 'returns all users' do
+      allow(User).to receive(:all).and_return(@users_list) #mocking
+      expect(controller.index).to eq(@users_list)
+    end
   end
 
   describe '#create' do
@@ -23,7 +32,6 @@ RSpec.describe UsersController, type: :controller  do
     context 'with invalid attributes' do
       it 'does not creates a user' do
         post :create, user: @invalid_params
-
         expect(User.count).to eq(0)
       end
       it 're renders the new view' do
@@ -31,7 +39,7 @@ RSpec.describe UsersController, type: :controller  do
         expect(response).to render_template :new
       end
     end
-    
+
   end
 
 end 
