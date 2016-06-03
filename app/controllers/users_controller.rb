21b4c114d716ad45629a167c4319ea4dd8da3798
arunnK <arunkumar7617@gmail.com>
2016-06-03 17:27:36 +0530

    just an edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
+  
   def new
     @user = User.new
   end
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
   private
    
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, 
+                                      :password_confirmation)
     end
     
 end
